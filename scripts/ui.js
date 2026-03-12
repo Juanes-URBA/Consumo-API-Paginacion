1ee359c27af579424b8d96d3acc34508709c3e3d
Juanes Urrego <024juanurrego@gmail.com>
2026-03-12 00:00:21 -0500

    Finaliza implementación de ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -2,16 +2,53 @@ import { getState, setState } from "./state.js";
 import { loadPokemon } from "./service.js";
 
 async function changePage(newPage){
-    setState("page",newPage)
+    setState("page", newPage)
     await  loadPokemon()
+    render()
 }
 
 function getPokemonCart(p){
-    const type = p.type[0].type.name
+    const type = p.types[0].type.name
     return `
     <div class="pokemon-card">
     <img src="${p.sprites.front_default}">
     <h2>#${p.id} ${p.name}</h2>
     <p class="type">${type}</p>
     </div>`
+}
+function renderPagination(){
+    const pagination = document.getElementById("pagination")
+    pagination.innerHTML = ""
+
+    const page = getState("page")
+    const total = getState("total")
+    const limit = getState("limit")
+
+    const totalPages = Math.ceil(total/limit)
+
+    for (let i = 0; i < totalPages; i++) {
+        const btn = document.createElement("button")
+        btn.textContent = i + 1
+
+        if (i === page) {
+            btn.classList.add("active")
+        }
+
+        btn.addEventListener("click",()=>{
+            changePage(i)
+        })
+        pagination.appendChild(btn)
+    }
+}
+
+export function render (){
+    const container = document.getElementById("container")
+    container.innerHTML = ""
+    const pokemon = getState("pokemon")
+
+    pokemon.forEach(p=>{
+        container.innerHTML += getPokemonCart(p)
+    })
+
+    renderPagination()
 }
